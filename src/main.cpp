793194749c10653dcf961b172bd9a736b01e52cf
ValeLG <pacolgishere@gmail.com>
2024-02-10 18:10:32 -0600

    fix betterEdit6 problems

--- src/main.cpp
+++ src/main.cpp
@@ -23,16 +23,20 @@ class $modify(EditorPauseLayerWithVolume,EditorPauseLayer) {
 		if(!EditorPauseLayer::init(p0))
 			return false;
 
-		// Move the guidelines-menu buttons to the top-menu.
+		// Move the guidelines-menu to the top.
 		CCNode* guidelinesButtons = this->getChildByID("guidelines-menu");
 		CCNode* topPosition = this->getChildByID("top-menu");
-		for(int i=0;i<=guidelinesButtons->getChildrenCount();i++)
-			switchChildToMenu(guidelinesButtons,0,topPosition);
-		topPosition->updateLayout();
+		guidelinesButtons->setPosition(topPosition->getPosition()); //I hope that no one use top-menu
 
 		// Modifications to the EditorPauseLayer layout.
 		auto winSize = CCDirector::get()->getWinSize();
-		guidelinesButtons->setContentSize({winSize.width,guidelinesButtons->getContentSize().height});
+		auto sliderMenu = CCMenu::create();
+		// New menu for BetterEditor compatibility
+		sliderMenu->setLayout(RowLayout::create());
+		sliderMenu->setID("sliders-menu");
+		sliderMenu->setContentSize({winSize.width,guidelinesButtons->getContentSize().height});
+		sliderMenu->setPositionY(sliderMenu->getContentSize().height/2 + 10);
+		this->addChild(sliderMenu);
 		CCNode* menuTemp = this->getChildByID("options-menu");
 		menuTemp->setLayout(
 			RowLayout::create()
@@ -48,6 +52,7 @@ class $modify(EditorPauseLayerWithVolume,EditorPauseLayer) {
 				->setAxis(Axis::Column)
 				->setAxisAlignment(AxisAlignment::End)
 				->setCrossAxisOverflow(false)
+				->setGap(4)
  		);
 		menuTemp->setPositionY(winSize.height-menuTemp->getContentSize().height/2-5);
 		menuTemp=this->getChildByID("settings-menu");
@@ -56,8 +61,8 @@ class $modify(EditorPauseLayerWithVolume,EditorPauseLayer) {
 		menuTemp->setPositionY(winSize.height-menuTemp->getContentSize().height/2+this->getChildByID("settings-menu")->getContentSize().height/2-5); // sorry
 
 		// Make the sliders.
-		Slider* musicSlider = Slider::create(guidelinesButtons, menu_selector(EditorPauseLayerWithVolume::onMusicChanged));
-		Slider* sfxSlider = Slider::create(guidelinesButtons, menu_selector(EditorPauseLayerWithVolume::onSfxChanged));
+		Slider* musicSlider = Slider::create(sliderMenu, menu_selector(EditorPauseLayerWithVolume::onMusicChanged));
+		Slider* sfxSlider = Slider::create(sliderMenu, menu_selector(EditorPauseLayerWithVolume::onSfxChanged));
 		// Change sizes and add value of the sliders.
 		getChildOfType<SliderTouchLogic>(musicSlider,0)->m_length=160;
 		getChildOfType<CCSprite>(musicSlider,0)->setScale(0.8);
@@ -71,11 +76,11 @@ class $modify(EditorPauseLayerWithVolume,EditorPauseLayer) {
 		float sliderX = getChildOfType<CCSprite>(musicSlider,0)->getContentSize().width * 0.8;
 		float sliderY = getChildOfType<SliderThumb>(getChildOfType<SliderTouchLogic>(sfxSlider,0),0)->getContentSize().height * 0.8 - 7.5;
 		float spacing = (winSize.width - (sliderX * 2 + sliderY))/2;
-		musicSlider->setPosition(spacing + sliderX / 2, guidelinesButtons->getPositionY()/2);
-		sfxSlider->setPosition(spacing + sliderX * 1.5 + sliderY, guidelinesButtons->getPositionY()/2);
-		// Add the sliders to guidelines-menu.
-		guidelinesButtons->addChild(musicSlider);
-		guidelinesButtons->addChild(sfxSlider);
+		musicSlider->setPosition(spacing + sliderX / 2, sliderMenu->getPositionY()/2);
+		sfxSlider->setPosition(spacing + sliderX * 1.5 + sliderY, sliderMenu->getPositionY()/2);
+		// Add the sliders to slider-menu.
+		sliderMenu->addChild(musicSlider);
+		sliderMenu->addChild(sfxSlider);
 		
 		//Make and add Labels "Music" and "SFX".
 		CCLabelBMFont* lblMusic = CCLabelBMFont::create("Music","bigFont.fnt");
@@ -84,10 +89,10 @@ class $modify(EditorPauseLayerWithVolume,EditorPauseLayer) {
 		lblSfx->setScale(0.5);
 		lblMusic->setAnchorPoint({0.5,0});
 		lblSfx->setAnchorPoint({0.5,0});
-		lblMusic->setPosition(spacing + sliderX / 2, guidelinesButtons->getPositionY()/2 + sliderY/2);
-		lblSfx->setPosition(spacing + sliderX * 1.5 + sliderY, guidelinesButtons->getPositionY()/2 + sliderY/2);
-		guidelinesButtons->addChild(lblMusic);
-		guidelinesButtons->addChild(lblSfx);
+		lblMusic->setPosition(spacing + sliderX / 2, sliderMenu->getPositionY()/2 + sliderY/2);
+		lblSfx->setPosition(spacing + sliderX * 1.5 + sliderY, sliderMenu->getPositionY()/2 + sliderY/2);
+		sliderMenu->addChild(lblMusic);
+		sliderMenu->addChild(lblSfx);
 		return true;
 	}
 
